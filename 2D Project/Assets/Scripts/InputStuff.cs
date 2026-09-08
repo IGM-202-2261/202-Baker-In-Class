c750dcdb0488d8ccdf914c7c00bf5bcc686f554e
Eric Baker <8997206+patches34@users.noreply.github.com>
2026-09-08 12:10:14 -0400

    week 3 day 1

---
+++ 2D Project/Assets/Scripts/InputStuff.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputStuff : MonoBehaviour
+{
+
+    public Vector2 movement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("Fire");
+        }*/
+
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Debug.Log(mousePos);
+    }
+
+    public void OnPlayerJump()
+    {
+        Debug.Log("jump");
+    }
+
+    public void OnBetterJump(InputAction.CallbackContext context)
+    {
+        if( context.performed )
+        {
+            Debug.Log("Better Jump");
+        }
+    }
+
+    public void OnPlayerMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+}
