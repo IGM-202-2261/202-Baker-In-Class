27ddd7c313fe78e005885ede8005ea45139e1cb4
Eric Baker <8997206+patches34@users.noreply.github.com>
2026-09-15 12:13:08 -0400

    week 4 random demos

--- 2D Project/Assets/Scripts/Spawner.cs
+++ 2D Project/Assets/Scripts/Spawner.cs
@@ -6,18 +6,31 @@ public class Spawner : MonoBehaviour
 
     public GameObject clone;
 
+    int spawnCount = 1000;
+
+    public Vector2 spawnMean;
+    public float spawnSTD;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        clone = Instantiate(prefab);
+        for(int i = 0; i < spawnCount; i++)
+        {
+            Vector3 spawnPos = new Vector2(Gaussian(spawnMean.x, spawnSTD), Gaussian(spawnMean.y, spawnSTD));
+
+            clone = Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    float Gaussian(float mean, float stdDev)
     {
-        if (clone == null)
-        {
-            Start();
-        }
+        float val1 = Random.Range(0f, 1f);
+        float val2 = Random.Range(0f, 1f);
+        float gaussValue =
+                 Mathf.Sqrt(-2.0f * Mathf.Log(val1)) *
+                 Mathf.Sin(2.0f * Mathf.PI * val2);
+        return mean + stdDev * gaussValue;
     }
+
 }
