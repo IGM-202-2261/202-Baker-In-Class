563b62ca09562cb22ddb9a6ab64f3910a415d339
Eric Baker <8997206+patches34@users.noreply.github.com>
2026-09-22 15:16:36 -0400

    week 5 vector demo

--- 2D Project/Assets/Scripts/Movement.cs
+++ 2D Project/Assets/Scripts/Movement.cs
@@ -8,36 +8,18 @@ public class Movement : MonoBehaviour
     public float playerSpeed;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //movement.x = Random.Range(-1f, 1f);
         //movement.y = Random.Range(-1f, 1f);
 
-        float randValue = Random.value;
 
-        if(randValue < .5)
-        {
-            movement = Vector2.up;
-        }
-        else if (randValue < (.5 + .2))
-        {
-            movement = Vector2.left;
-        }
-        else if(randValue < (.5 + .2 + .2))
-        {
-            movement = Vector2.right;
-        }
-        else
-        {
-            movement = Vector2.down;
-        }
-
-        transform.Translate(movement * Time.deltaTime * playerSpeed);
+        transform.Translate(movement * Time.fixedDeltaTime * playerSpeed);
     }
 
     public void OnPlayerMove(InputAction.CallbackContext context)
     {
-        //movement = context.ReadValue<Vector2>();
+        movement = context.ReadValue<Vector2>();
 
         
     }
